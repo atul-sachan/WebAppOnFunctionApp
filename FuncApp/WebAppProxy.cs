0ad229f1cfa7332dd6db6917a35d515498621462
Atul Sachan <atul.sachan.dotnet@outlook.com>
2020-03-29 02:15:38 +0530

    Add project files.

---
+++ FuncApp/WebAppProxy.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder.Internal;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ObjectPool;
+using WebApp;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
+
+namespace FuncApp
+{
+    public static class WebAppProxy
+    {
+        [FunctionName(nameof(WebAppProxy))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "get", "post","put", "patch",
+                Route = "{*any}")]
+            HttpRequest req,
+            ExecutionContext context,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var configRoot = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var hostingEnvironment = new HostingEnvironment()
+            {
+                ContentRootPath = context.FunctionAppDirectory
+            };
+
+            /* Add required services into DI container */
+            var config = configRoot.GetWebJobsRootConfiguration();
+            var services = new ServiceCollection();
+            services.AddSingleton<DiagnosticSource>(new DiagnosticListener("Microsoft.AspNetCore"));
+            services.AddSingleton<ObjectPoolProvider>(new DefaultObjectPoolProvider());
+            services.AddSingleton<IHostingEnvironment>(hostingEnvironment);
+            services.AddSingleton<IConfiguration>(config);
+
+            /* Instantiate standard ASP.NET Core Startup class */
+            var startup = new Startup(config);
+
+            /* Add web app services into DI container */
+            startup.ConfigureServices(services);
+
+            /* Initialize DI container */
+            var serviceProvider = services.BuildServiceProvider();
+
+            /* Initialize Application builder */
+            var appBuilder = new ApplicationBuilder(serviceProvider, new FeatureCollection());
+
+            /* Configure the HTTP request pipeline */
+            startup.Configure(appBuilder, hostingEnvironment);
+
+            /* Build request handling function */
+            var requestHandler = appBuilder.Build();
+
+            /* Set DI container for HTTP Context */
+            req.HttpContext.RequestServices = serviceProvider;
+
+            /* Handle HTTP request */
+            await requestHandler(req.HttpContext);
+
+            /* This dummy result does nothing, HTTP response is already set by requestHandler */
+            return new EmptyResult();
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddLogging()
+                .AddMvc()
+                // Explicitly add WebbApplication assembly as application part
+                // This is required because WebApplication isn't executing assembly
+                // when being hosted as Azure Function
+                .AddApplicationPart(Assembly.Load("WebApp"));
+
+            // Register the Swagger services
+            services.AddSwaggerDocument();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
